aaf5ada2eaee13561091955cc53e698cda2cddaf
Banjoko Kazeem <58645797+kazmanbanj@users.noreply.github.com>
2020-05-23 15:15:49 -0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="about">
-      <p class="abouthead">About Us</p>
+      <h1 class="abouthead">About Us</h1>
       <p class="abouttext"><strong>LearnNow</strong> is a self study app which lets you self study at any place, anywhere or anytime at your convenience. It is basically an app for students in nursery, primary and secondary schools (even useful for students in universities and polytechnics). This app is truly a tool for discovering unique learning skills and has a blend of different contents as far as education is concerned.<br>
       Our users can count on us to expect the unexpected; we are fun, elegant, know how to add technology to everyday learning and also add spice to your learning ability.<br>
       Browse through our app now and see all that we have to offer.<strong> LearnNow</strong> app is not just a learning app but a special one which will blow your mind away with its features.</p>
@@ -23,16 +23,26 @@ export default {
     text-decoration: underline;
   }
 
+  p .abouthead {
+    margin-bottom: 8px !important;
+  }
+
   .about {
-    margin-top: 20px;
-    /* margin-bottom: 100px; */
+    margin-top: 15px;
   }
 
   .abouttext {
     font-size: 25px;
     line-height: 65px;
     color: rgb(22, 21, 21);
-    margin-top: 10px;
+    padding: 5px;
+    /* margin-top: 3px; */
   }
 
+  @media only screen and (max-width: 768px) {
+    .abouttext {
+    font-size:20px;
+    line-height: 30px;
+  }
+  }
 </style>
