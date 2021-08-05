d23123d8fe6b65d7ca788bc9d94ae0f2dfad7024
Banjoko Kazeem <banjoko10@gmail.com>
2021-08-05 21:02:54 +0100

    removed the removables

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
